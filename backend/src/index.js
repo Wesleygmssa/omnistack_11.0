a1502ed9edf415c6032a163c59ae9b238db3a7ce
Wesley Guerra <wesleyguerra9@gmail.com>
2020-03-25 12:24:25 -0300

    Back-end da aplicação

---
+++ backend/src/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const routes = require('./routes')
+
+const app = express();
+const cors = require('cors');
+
+
+
+
+
+
+app.use(cors());     // origin:'http://meuapp.com'
+app.use(express.json()); //utlização do formato json
+app.use(routes)
+app.listen(3333)
+
+
+/* 
+
+Query Params: Parâmetros nomeados enviados na rota "? (filtros, paginação)
+Route Params: Parâmetros utilizados para identificar recursos
+Resquest Body: cirpo da requisiçãoi, utlizado para criar ou alterar recurso
+
+*/
+
+/* SQL: MySQL, SQLLite, PostgreSQl, Oracle, Microsft Sql Server
+noSQL: MongoDb, CouchDB */
+
+
+
+
+
